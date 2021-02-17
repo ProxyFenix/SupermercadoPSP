edb34658b21bfef2f03d2ab01135e31b9c47c29e
Proxy <manuel.jurado@metadata.es>
2021-02-17 10:02:14 +0100

    Correo funcional, o debería al menos

--- FinalPSP/src/Util/CorreoJava.java
+++ FinalPSP/src/Util/CorreoJava.java
@@ -16,16 +16,18 @@ import javax.mail.internet.*;
  * con modificaciones
  * 
  * 15/02/21 En proceso
+ * 16/02/21 Listo
+ * 17/02/21 Arreglado el tema de correo no mandado
  */
 public class CorreoJava {
 
 	public void mandarCorreo(String nombreProducto, String hora, int precioProveedor) {
 
-		
-		String host = "localhost";
-		final String user = "SupermercadosWhiteBase@Argama.com";// Correo cualquiera
-		// final String passwd = "LaBebesitaBebelin";
-		String toUser = "";
+		String host = "mail.javatpoint.com";
+		final String user = "SupermercadosWhiteBase@gmail.com";// Correo cualquiera
+		// Con tiempo, aquí se añadiría una encriptación de la contraseña, y coger la
+		// contraseña de un fichero
+		final String passwd = "py8gi6i5+";
 
 		String finalToUser = "";
 		String firstToUser = "";
@@ -34,7 +36,7 @@ public class CorreoJava {
 		File correoXML = new File(resource.getPath());
 		System.out.println(correoXML);
 
-		//Aqui antes habia un StringBuilder, pero era menos eficiente
+		// Aqui antes habia un StringBuilder, pero era menos eficiente
 		String lineaProto = "";
 
 		// Encontramos el email en el XML
@@ -63,7 +65,11 @@ public class CorreoJava {
 		// Get the session object
 		Properties properties = System.getProperties();
 		properties.setProperty("mail.smtp.host", host);
-		Session session = Session.getDefaultInstance(properties);
+		Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(user, passwd);
+			}
+		});
 		// Compose the message
 		try {
 			MimeMessage message = new MimeMessage(session);
@@ -75,7 +81,7 @@ public class CorreoJava {
 
 			// send the message
 			Transport.send(message);
-			System.out.println(message);
+			System.out.println(message.toString());
 			System.out.println("message sent successfully...");
 
 		} catch (MessagingException e) {
